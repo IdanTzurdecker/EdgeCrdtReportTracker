fbc562ce6fdcf24bab99295fe73bebc75dcb1713
Idan Tzurdecker <idan_tzur@outlook.com>
2025-11-20 23:31:01 -0500

    clean up notes from research

--- Models/AuditLog.cs
+++ Models/AuditLog.cs
@@ -102,17 +102,3 @@ public class AuditLog
     }
 }
 
-
-
-
-// government compliance audit log should follow-
-// "NIST SP 800-53 Control AU-3 requirements". this need to cover
-/**
-    1.	What event occurred
-    2.	When it occurred
-    3.	Where it occurred
-    4.	The source of the event
-    5.	The outcome of the event
-    6.	The identity of the user/process/system associated with the event (when applicable)
-*/
-
